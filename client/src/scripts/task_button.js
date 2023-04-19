a73863417ece281e507d516d48ff3c369cfffde5
M Dattis <38086897+MssTake@users.noreply.github.com>
2023-04-18 23:54:36 -0400

    Update task_button.js

--- client/src/scripts/task_button.js
+++ client/src/scripts/task_button.js
@@ -10,9 +10,25 @@ for (let button of buttons) {
 buttons = document.querySelectorAll('.task_add_button');
 
 for (let button of buttons) {
+
+    // setting up ID for upcoming task
+    let taskCounter = 1;
+    let taskID = taskCounter + taskCounter.charCodeAt(index);
+    
+    // setting up task name
+    let task name
+    
     button.onclick = () => {
+        button.setProperty(id,taskID);
+        button.setName(name,taskName);
+        button task list 
         location.href = 'list.html';
     };
+    
+    // incrementing number and/or letter for ID
+    if (taskCounter % 26 = 1) {
+    }
+    taskCounter++;
 }
 
 let button = document.querySelector('.submit_button_placeholder');
