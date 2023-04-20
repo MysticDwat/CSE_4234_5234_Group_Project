b296b8b55ebe192fc258c9f00e0010bd522e8bc9
MysticDwat <william.hottenstein19@gmail.com>
2023-04-19 20:14:13 -0400

    Firebase Setup File

---
+++ client/src/scripts/firebase_setup.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAvtpZ3GrqJtCFWwV886jYRf-auKgJw9JQ",
+    authDomain: "cse4234-5234.firebaseapp.com",
+    projectId: "cse4234-5234",
+    storageBucket: "cse4234-5234.appspot.com",
+    messagingSenderId: "480566440924",
+    appId: "1:480566440924:web:208a368908c9c019109519",
+    measurementId: "G-EFB9BVWLL3"
+};
+
+const app = initializeApp(firebaseConfig);
