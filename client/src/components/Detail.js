fbe261815ffc1998fee6387739eea79adf2cde02
MysticDwat <william.hottenstein19@gmail.com>
2023-05-03 16:02:31 -0400

    Fixed Task View Date Formatting

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -10,9 +10,16 @@ export default function Detail(){
     const user = useContext(UserContext);
     const tasks = useGetTasks(user !== null ? user.uid : 'public', [task_id, false]);
     
+    let format_date = () => {
+        if(!tasks.due_date){return '';}
+        let date = new Date(tasks.due_date);
+        date = (new Date(date.getTime() - date.getTimezoneOffset() * 60000).toUTCString()).slice(0, -4);
+        return date;
+    }
+
     return(
         <main>
-            <TaskDetailed name={tasks.name} description={tasks.description} category={tasks.category} deadline={tasks.due_date} status={tasks.status}/>
+            <TaskDetailed name={tasks.name} description={tasks.description} category={tasks.category} deadline={format_date()} status={tasks.status}/>
         </main>
     );
 }
