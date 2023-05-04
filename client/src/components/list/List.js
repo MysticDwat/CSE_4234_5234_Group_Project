78d34c0e2de13d0b13a445ecc2e71d8f23bc5d9a
MysticDwat <william.hottenstein19@gmail.com>
2023-05-04 13:46:50 -0400

    Added No Items Found Result

--- client/src/components/list/List.js
+++ client/src/components/list/List.js
@@ -11,6 +11,8 @@ export default function List(){
     const [active_id, set_active_id] = useState(0);
     const [refresh, set_refresh] = useState(false);
     const [search_name, set_search_name] = useState('');
+    const [tabs, set_tabs] = useState([]);
+    const [pages, set_pages] = useState([]);
 
     const user = useContext(UserContext);
     const tasks = useGetTasks(user !== null ? user.uid : 'public',[null, true, refresh]);
@@ -35,7 +37,7 @@ export default function List(){
     let filter_tabs = () => {
         let filtered_categories = filter_search_results();
 
-        if (!filtered_categories.filter(x => x._id === active_id).length){
+        if (!filtered_categories.filter(x => x._id === active_id).length && filtered_categories.length){
             set_active_id(filtered_categories[0]._id);
         }
 
@@ -59,6 +61,39 @@ export default function List(){
         );
     }
 
+    useEffect(() => {
+        let new_category_tasks = {};
+
+        for (let x of categories) {
+            new_category_tasks[x._id] = tasks.filter(y => y.category_id === x._id && (search_name === '' || y.name.includes(search_name)));
+        }
+
+        new_category_tasks = Object.fromEntries(Object.entries(new_category_tasks).filter(x => x[1].length > 0));
+        
+        let filtered_categories = categories.filter(x => search_name === '' || Object.keys(new_category_tasks).includes(x._id));
+
+        if (!filtered_categories.filter(x => x._id === active_id).length && filtered_categories.length){
+            set_active_id(filtered_categories[0]._id);
+        }
+
+        set_tabs(filtered_categories.map((x,i) => 
+            <Tab id={`tab-${x._id}`} name={x.name} set_active_id={set_active_id} tab_key={x._id} key={x.name} />
+        ));
+        
+        set_pages(filtered_categories.length ? filtered_categories.map((x,i) => 
+            <Page 
+                id={`content-${x._id}`} 
+                category={x.name} 
+                active={active_id} 
+                content_key={x._id} 
+                key={x.name}
+                tasks={tasks.filter(y => y.category_id === x._id && (search_name === '' || y.name.includes(search_name)))}
+                toggle_refresh={toggle_refresh}
+            />) :
+            <div>No Items Found</div>
+        );
+    },[JSON.stringify(categories), JSON.stringify(tasks), search_name]);
+
     return(
         <main>
             {tasks.length ? 
@@ -68,11 +103,11 @@ export default function List(){
                     </div>
 
                     <div className="tabs">
-                        {filter_tabs()}
+                        {tabs}
                     </div>
 
                     <div className="content">
-                        {filter_pages()}
+                        {pages}
                     </div>
                 </div> 
                 :
